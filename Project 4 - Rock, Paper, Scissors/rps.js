b2bbbd3a89c5ed5b49d8e4119aaf9bbb49574899
Joseph Adusei <99763863+jsph-jay@users.noreply.github.com>
2022-06-24 10:59:09 +0100

    Project 4 - Rock Paper Scissors
    A very basic version of Rock, Paper, Scissors. Will like to come back and see how I can simplify the code for the game logic; didn't really follow DRY as much during this codealong. 
    
    New JS properties: e.target.id, forEach
    
    Played along with some of the CSS aspects of the project. Was able to find some nice Button effects that I can definitely see myself using in the future when I geet momoroe confident in my abilities. Making progress nonetheless

---
+++ Project 4 - Rock, Paper, Scissors/rps.js
@@ -0,0 +1,62 @@
+// Skills that I would have used by the end of the project: getElementbyID, querySelectorAll, forEach, addEventListener, innerHTML, e.target.id, Mathfloor and Math.random
+
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+
+const possibleChoices = document.querySelectorAll('button')
+
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*possibleChoices.length + 1)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw"
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = "You Win"
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = "You Lost"
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = "You Win"
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = "You Lost"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = "You Win"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = "You Lost"
+    }
+
+    resultDisplay.innerHTML = result
+}
